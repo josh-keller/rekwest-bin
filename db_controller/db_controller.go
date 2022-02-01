16806be2a9c992af5c931ae28c461a0b861ce6b8
Stephen Gontzes <gontzess@gmail.com>
2022-02-01 17:22:51 -0500

    initial db_controller

---
+++ db_controller/db_controller.go
@@ -0,0 +1,44 @@
+package dbcontroller
+
+import (
+  "context"
+  "fmt"
+  "log"
+  "github.com/joho/godotenv/autoload"
+	// "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	// "go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectMongo() {
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
+  // Set client options
+  clientOptions := options.Client().ApplyURI(os.Getenv("MONGODB_URI"))
+
+  // Connect to MongoDB
+  client, err := mongo.Connect(context.TODO(), clientOptions)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  // Check the connection
+  err = client.Ping(context.TODO(), nil)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Println("Connected to MongoDB!")
+}
+
+
+// uri := os.Getenv("MONGODB_URI")
+
+func AddBin() {
+
+}
