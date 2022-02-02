60f2d50dfe7e53382ccc970b22395ff57c7647ae
Stephen Gontzes <gontzess@gmail.com>
2022-02-02 17:29:07 -0500

    db controller merging

--- db_controller/db_controller.go
+++ db_controller/db_controller.go
@@ -1,14 +1,15 @@
 package db_controller
 
 import (
-  "context"
-  "fmt"
-  "log"
-  "os"
-  "time"
-  "github.com/joho/godotenv"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
-  "go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -18,164 +19,206 @@ var bins *mongo.Collection
 
 // clean up structs
 type Bin struct {
-  BinId string
-  Created_at string // timestamp
-  Rekwests []Rekwest
+	BinId      string
+	Created_at string // timestamp
+	Rekwests   []Rekwest
 }
 
 type Rekwest struct {
-  RekwestId string
-  Method string
-  Host string
-  Path string
-  Created string // timestamp
-  Parameters map[string]string
-  Headers map[string]string
-  Body string
-  Raw string
+	RekwestId  string
+	Method     string
+	Host       string
+	Path       string
+	Created    string // timestamp
+	Parameters map[string]string
+	Headers    map[string]string
+	Body       string
+	Raw        string
 }
 
 func Connect() {
-  if client != nil {
-    return
-  }
+	if client != nil {
+		return
+	}
 
-  err := godotenv.Load()
-  if err != nil {
-    log.Fatal("Error loading .env file")
-  }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-  // clientOptions := options.Client().ApplyURI(os.Getenv("MONGODB_URI"))
-  clientOptions := options.Client().ApplyURI(os.Getenv("MONGODB_TEST_URI"))
+	// clientOptions := options.Client().ApplyURI(os.Getenv("MONGODB_URI"))
+	clientOptions := options.Client().ApplyURI(os.Getenv("MONGODB_TEST_URI"))
 
-  client, err = mongo.Connect(context.TODO(), clientOptions)
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  err = client.Ping(context.TODO(), nil)
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  fmt.Println("MongoDB connected")
-  bins = client.Database("rekwest-bin").Collection("bins")
-}
-
-func Disconnect() {
-  if client == nil {
-    return
-  }
-
-  err := client.Disconnect(context.TODO())
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  fmt.Println("MongoDB disconnected")
-}
-
-// update how we create the binId / url hash
-func NewBin() (Bin, string) {
-  newBin := Bin{
-    BinId: "",
-    Created_at: time.Now().GoString(),
-    Rekwests: make([]Rekwest, 0),
-  }
-  results, err := bins.InsertOne(context.TODO(), newBin)
+	client, err = mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-  fmt.Println(newBin)
-  newBin.BinId = results.InsertedID.(primitive.ObjectID).Hex()
-  fmt.Println(newBin)
-  return newBin, newBin.BinId
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("MongoDB connected")
+	bins = client.Database("rekwest-bin").Collection("bins")
+}
+
+func Disconnect() {
+	if client == nil {
+		return
+	}
+
+	err := client.Disconnect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("MongoDB disconnected")
+}
+
+// update how we create the binId / url hash
+func NewBin() (Bin, string) {
+	newBin := Bin{
+		BinId:      "",
+		Created_at: time.Now().GoString(),
+		Rekwests:   make([]Rekwest, 0),
+	}
+	results, err := bins.InsertOne(context.TODO(), newBin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// fmt.Println(newBin)
+	newBin.BinId = results.InsertedID.(primitive.ObjectID).Hex()
+	// fmt.Println(newBin)
+	return newBin, newBin.BinId
 }
 
 func FindBin(binId string) (Bin, bool) {
-  objectID, err := primitive.ObjectIDFromHex(binId)
-  if err != nil {
-    return Bin{}, false
-  }
-  filter := bson.D{{"_id", objectID}}
-  var bin Bin
+	objectID, err := primitive.ObjectIDFromHex(binId)
+	if err != nil {
+		return Bin{}, false
+	}
+	filter := bson.D{{"_id", objectID}}
+	var bin Bin
 	err = bins.FindOne(context.TODO(), filter).Decode(&bin)
 	if err != nil {
-    // // check if these err checks are needed
+		// // check if these err checks are needed
 		// ErrNoDocuments means that the filter did not match any documents in
 		// the collection.
 		// if err == mongo.ErrNoDocuments {
 		// 	return Bin{}, true
 		// } else {
-    //   log.Fatal(err)
-    //   return Bin{}, false
-    // }
+		//   log.Fatal(err)
+		//   return Bin{}, false
+		// }
 
-    return Bin{}, false
+		return Bin{}, false
 	}
 
-  fmt.Println(bin)
-  bin.BinId = binId
-  fmt.Println(bin)
+	fmt.Println(bin)
+	bin.BinId = binId
+	fmt.Println(bin)
 
-  return bin, true
+	return bin, true
 }
 
 func GetAllBins() {
-  var results []Bin
-  findOptions := options.Find()
+	var results []Bin
+	findOptions := options.Find()
 
-  cursor, err := bins.Find(context.TODO(), bson.D{{}}, findOptions)
-  if err != nil {
-    log.Fatal(err)
-  }
+	cursor, err := bins.Find(context.TODO(), bson.D{{}}, findOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  for cursor.Next(context.TODO()) {
-    var elem Bin
-    err := cursor.Decode(&elem)
-    if err != nil {
-      log.Fatal(err)
-    }
+	for cursor.Next(context.TODO()) {
+		var elem Bin
+		err := cursor.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-    results = append(results, elem)
-  }
+		results = append(results, elem)
+	}
 
-  if err := cursor.Err(); err != nil {
-    log.Fatal(err)
-  }
+	if err := cursor.Err(); err != nil {
+		log.Fatal(err)
+	}
 
-  // not returning anything, just printing
-  fmt.Println(results)
-  cursor.Close(context.TODO())
+	// not returning anything, just printing
+	fmt.Println(results)
+	cursor.Close(context.TODO())
 }
 
 func AddRekwest(binId string, rekwest Rekwest) bool {
-  bin, success := FindBin(binId)
-  if !success {
-    return false
-  }
+	bin, success := FindBin(binId)
+	if !success {
+		return false
+	}
 
-  fmt.Println(bin)
+	fmt.Println(bin)
 
-  objectID, err := primitive.ObjectIDFromHex(binId)
-  if err != nil {
-    return false
-  }
+	objectID, err := primitive.ObjectIDFromHex(binId)
+	if err != nil {
+		return false
+	}
 
-  // add slicing functionality
-  bin.Rekwests = append(bin.Rekwests, rekwest)
+	// add slicing functionality
+	bin.Rekwests = append(bin.Rekwests, rekwest)
 
-  filter := bson.D{{"_id", objectID}}
-  update := bson.D{{"$set", bson.D{{"rekwests", bin.Rekwests}}}}
-  result, err := bins.UpdateOne(context.TODO(), filter, update)
-  if err != nil {
-    log.Fatal(err)
-  }
+	filter := bson.D{{"_id", objectID}}
+	update := bson.D{{"$set", bson.D{{"rekwests", bin.Rekwests}}}}
+	result, err := bins.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  if result.MatchedCount == 0 {
-    return false
-  }
+	if result.MatchedCount == 0 {
+		return false
+	}
 
-  return true
+	return true
 }
+
+/*
+
+	"github.com/wboard82/rekwest-bin/db_controller"
+
+var testBin = db_controller.Bin{
+	BinId:      "",
+	Created_at: time.Now().GoString(), // timestamp
+	Rekwests:   make([]db_controller.Rekwest, 20),
+}
+
+var testRekwest = db_controller.Rekwest{
+	RekwestId:  "",
+	Method:     "POST",
+	Host:       "316e-174-81-238-56.ngrok.io",
+	Path:       "/r/",
+	Created:    time.Now().GoString(), // timestamp
+	Parameters: nil,
+	Headers: map[string]string{
+		"User-Agent":        "curl/7.68.0",
+		"Content-Length":    "28",
+		"Accept":            "*\/*",
+		"Accept-Encoding":   "gzip",
+		"Content-Type":      "application/json",
+		"X-Forwarded-For":   "192.222.245.48",
+		"X-Forwarded-Proto": "https",
+	},
+	Body: `{"dragons": "are dangerous"}`,
+	Raw:  "hi im a raw rekwest",
+}
+
+func main() {
+	db_controller.Connect()
+	defer db_controller.Disconnect()
+	bin, binId := db_controller.NewBin()
+	fmt.Println(bin, binId, bin.BinId)
+	bin, success := db_controller.FindBin(binId)
+	fmt.Println(bin, success)
+	db_controller.GetAllBins()
+	db_controller.AddRekwest(binId, testRekwest)
+}
+*/
--- main.go
+++ main.go
@@ -2,184 +2,146 @@ package main
 
 import (
 	"fmt"
-	// "log"
-	// "math/rand"
-	// "net/http"
-	// "net/http/httputil"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/http/httputil"
 	"time"
-  "github.com/wboard82/rekwest-bin/db_controller"
 )
 
-var testBin = db_controller.Bin{
-  BinId: "",
-  Created_at: time.Now().GoString(), // timestamp
-  Rekwests: make([]db_controller.Rekwest, 20),
+// Possible letters for the random ID
+var letters = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+// Right now the BinStore encapsulates a randome number generator, for...reasons
+// Not sure if this is a good idea or not, but it's how it's working for now
+type BinStore struct {
+	Bins    map[string][]string
+	randGen *rand.Rand
 }
 
-var testRekwest = db_controller.Rekwest{
-  RekwestId: "",
-  Method: "POST",
-  Host: "316e-174-81-238-56.ngrok.io",
-  Path: "/r/",
-  Created: time.Now().GoString(), // timestamp
-  Parameters: nil,
-  Headers: map[string]string{
-    "User-Agent": "curl/7.68.0",
-    "Content-Length": "28",
-    "Accept": "*/*",
-    "Accept-Encoding": "gzip",
-    "Content-Type": "application/json",
-    "X-Forwarded-For": "192.222.245.48",
-    "X-Forwarded-Proto": "https",
-  },
-  Body: `{"dragons": "are dangerous"}`,
-  Raw: "hi im a raw rekwest",
+// NewBinStore creates a new BinStore and returns a reference to it
+// It also seeds the random number generator
+func NewBinStore() *BinStore {
+	source := rand.NewSource(time.Now().UnixNano())
+	gen := rand.New(source)
+
+	return &BinStore{make(map[string][]string), gen}
 }
 
-func main () {
-  db_controller.Connect()
-  defer db_controller.Disconnect()
-  bin, binId := db_controller.NewBin()
-  fmt.Println(bin, binId, bin.BinId)
-  bin, success := db_controller.FindBin(binId)
-  fmt.Println(bin, success)
-  db_controller.GetAllBins()
-  db_controller.AddRekwest(binId, testRekwest)
+// NewBin creates a new empty bin in the store
+func (store *BinStore) NewBin() string {
+	// Generate random id: could be factored out
+	b := make([]rune, 8)
+	for i := range b {
+		b[i] = letters[store.randGen.Intn(len(letters))]
+	}
+
+	result := string(b)
+	store.Bins[result] = []string{}
+
+	return result
 }
 
-// // Possible letters for the random ID
-// var letters = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-//
-// // Right now the BinStore encapsulates a randome number generator, for...reasons
-// // Not sure if this is a good idea or not, but it's how it's working for now
-// type BinStore struct {
-// 	Bins    map[string][]string
-// 	randGen *rand.Rand
-// }
-//
-// // NewBinStore creates a new BinStore and returns a reference to it
-// // It also seeds the random number generator
-// func NewBinStore() *BinStore {
-// 	source := rand.NewSource(time.Now().UnixNano())
-// 	gen := rand.New(source)
-//
-// 	return &BinStore{make(map[string][]string), gen}
-// }
-//
-// // NewBin creates a new empty bin in the store
-// func (store *BinStore) NewBin() string {
-// 	// Generate random id: could be factored out
-// 	b := make([]rune, 8)
-// 	for i := range b {
-// 		b[i] = letters[store.randGen.Intn(len(letters))]
-// 	}
-//
-// 	result := string(b)
-// 	store.Bins[result] = []string{}
-//
-// 	return result
-// }
-//
-// // GetBin gets the bin with the id of binName. It returns an array of
-// // strings (the requests) and a bool indicating whether the bin exists
-// func (store BinStore) GetBin(binName string) ([]string, bool) {
-// 	bin, exists := store.Bins[binName]
-// 	return bin, exists
-// }
-//
-// // AddRekwest adds a request to the given bin. It returns false if the bin
-// // does not exist
-// func (store *BinStore) AddRekwest(binName string, rekwest string) bool {
-// 	_, exists := store.Bins[binName]
-// 	if !exists {
-// 		return false
-// 	}
-//
-// 	binSize := len(store.Bins[binName])
-//
-// 	if binSize >= 20 {
-// 		store.Bins[binName] = store.Bins[binName][binSize+1-20:]
-// 	}
-//
-// 	store.Bins[binName] = append(store.Bins[binName], rekwest)
-// 	return true
-// }
-//
-// var binStore = NewBinStore()
-//
-// func main() {
-// 	http.HandleFunc("/r/", binHandler)
-// 	http.HandleFunc("/", rootHandler)
-// 	log.Fatal(http.ListenAndServe(":8080", nil))
-// }
-//
-// func rootHandler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "<h1>Welcome to Rekwest Bin</h1><form method='POST' action='/r/'><button type='submit'>Create a bin</button></form>")
-// }
-//
-// func binHandler(w http.ResponseWriter, r *http.Request) {
-// 	// The POST route creates a new bin and redirects to the inspect page
-// 	if r.Method == "POST" {
-// 		binName := binStore.NewBin()
-//
-// 		http.Redirect(w, r, "/r/"+binName+"?inspect", 302)
-// 		return
-// 	}
-//
-// 	// This grabs the part after /r/ in the path
-// 	hash := r.URL.Path[len("/r/"):]
-// 	// Put the full link together here to be displayed on a landing page
-// 	binAddress := fmt.Sprintf("http://%s/r/%s", r.Host, hash)
-//
-// 	// If there is a query "inspect", show all the requests
-// 	if r.URL.RawQuery == "inspect" {
-// 		rekwests, exists := loadRequest(hash)
-//
-// 		if !exists {
-// 			http.NotFound(w, r)
-// 			return
-// 		}
-//
-// 		fmt.Fprintf(w, "<h1>Here are your rekwests:</h1>")
-// 		if len(rekwests) == 0 {
-// 			fmt.Fprintf(w, "<h2>No rekwests</h2>")
-// 			fmt.Fprintf(w, "<p>Make a request to: %s", binAddress)
-// 			fmt.Fprintf(w, "<p>View request at: %s", binAddress+"?inspect")
-// 		}
-//
-// 		for i, rekwest := range rekwests {
-// 			fmt.Fprintf(w, "<h2>Rekwest %d</h2><p>%s</p>", i+1, rekwest)
-// 		}
-// 		// Otherwise, save the request (if the bin exists)
-// 	} else {
-// 		dump, err := httputil.DumpRequest(r, true)
-//
-// 		if err != nil {
-// 			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
-// 			return
-// 		}
-//
-// 		dump = append([]byte("<pre>"), dump...)
-// 		dump = append([]byte(dump), []byte("</pre>")...)
-//
-// 		requesterIP := r.Header.Get("X-Forwarded-For")
-//
-// 		if saveRequest(hash, dump) {
-// 			fmt.Fprintf(w, "<h1>Request saved</h1><p>%s</p>", requesterIP)
-// 			fmt.Fprintf(w, "<p><a href=%s>View requests</a>", binAddress+"?inspect")
-// 		} else {
-// 			http.NotFound(w, r)
-// 		}
-// 	}
-// }
-//
-// // Helpers - these could be extracted out along with the BinStore
-// func saveRequest(hash string, rekwest []byte) bool {
-// 	success := binStore.AddRekwest(hash, string(rekwest))
-// 	return success
-// }
-//
-// func loadRequest(hash string) ([]string, bool) {
-// 	bins, exists := binStore.GetBin(hash)
-// 	return bins, exists
-// }
+// GetBin gets the bin with the id of binName. It returns an array of
+// strings (the requests) and a bool indicating whether the bin exists
+func (store BinStore) GetBin(binName string) ([]string, bool) {
+	bin, exists := store.Bins[binName]
+	return bin, exists
+}
+
+// AddRekwest adds a request to the given bin. It returns false if the bin
+// does not exist
+func (store *BinStore) AddRekwest(binName string, rekwest string) bool {
+	_, exists := store.Bins[binName]
+	if !exists {
+		return false
+	}
+
+	binSize := len(store.Bins[binName])
+
+	if binSize >= 20 {
+		store.Bins[binName] = store.Bins[binName][binSize+1-20:]
+	}
+
+	store.Bins[binName] = append(store.Bins[binName], rekwest)
+	return true
+}
+
+var binStore = NewBinStore()
+
+func main() {
+	http.HandleFunc("/r/", binHandler)
+	http.HandleFunc("/", rootHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Welcome to Rekwest Bin</h1><form method='POST' action='/r/'><button type='submit'>Create a bin</button></form>")
+}
+
+func binHandler(w http.ResponseWriter, r *http.Request) {
+	// The POST route creates a new bin and redirects to the inspect page
+	if r.Method == "POST" {
+		binName := binStore.NewBin()
+
+		http.Redirect(w, r, "/r/"+binName+"?inspect", 302)
+		return
+	}
+
+	// This grabs the part after /r/ in the path
+	hash := r.URL.Path[len("/r/"):]
+	// Put the full link together here to be displayed on a landing page
+	binAddress := fmt.Sprintf("http://%s/r/%s", r.Host, hash)
+
+	// If there is a query "inspect", show all the requests
+	if r.URL.RawQuery == "inspect" {
+		rekwests, exists := loadRequest(hash)
+
+		if !exists {
+			http.NotFound(w, r)
+			return
+		}
+
+		fmt.Fprintf(w, "<h1>Here are your rekwests:</h1>")
+		if len(rekwests) == 0 {
+			fmt.Fprintf(w, "<h2>No rekwests</h2>")
+			fmt.Fprintf(w, "<p>Make a request to: %s", binAddress)
+			fmt.Fprintf(w, "<p>View request at: %s", binAddress+"?inspect")
+		}
+
+		for i, rekwest := range rekwests {
+			fmt.Fprintf(w, "<h2>Rekwest %d</h2><p>%s</p>", i+1, rekwest)
+		}
+		// Otherwise, save the request (if the bin exists)
+	} else {
+		dump, err := httputil.DumpRequest(r, true)
+
+		if err != nil {
+			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
+
+		dump = append([]byte("<pre>"), dump...)
+		dump = append([]byte(dump), []byte("</pre>")...)
+
+		requesterIP := r.Header.Get("X-Forwarded-For")
+
+		if saveRequest(hash, dump) {
+			fmt.Fprintf(w, "<h1>Request saved</h1><p>%s</p>", requesterIP)
+			fmt.Fprintf(w, "<p><a href=%s>View requests</a>", binAddress+"?inspect")
+		} else {
+			http.NotFound(w, r)
+		}
+	}
+}
+
+// Helpers - these could be extracted out along with the BinStore
+func saveRequest(hash string, rekwest []byte) bool {
+	success := binStore.AddRekwest(hash, string(rekwest))
+	return success
+}
+
+func loadRequest(hash string) ([]string, bool) {
+	bins, exists := binStore.GetBin(hash)
+	return bins, exists
+}
